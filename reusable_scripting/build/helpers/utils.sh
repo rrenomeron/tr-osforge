c8ba86c2210bd7fe28a86a99c1b5acd22da055d4
Rich Renomeron <rrenomeron@gmail.com>
2026-03-13 12:45:56 -0400

    feat: better OS detection

--- reusable_scripting/build/helpers/utils.sh
+++ reusable_scripting/build/helpers/utils.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/bash
 
-is_fedora() {
-    FEDORA_VERSION=$(rpm -E %fedora)
-    if [ "$FEDORA_VERSION" == "%fedora" ]; then
-        return 1
-    else
-        return 0
-    fi
-}
-
-is_alma() {
+get_base_os() {
     ALMA_VERSION=$(rpm -E %almalinux)
-    if [ "$ALMA_VERSION" == "%almalinux" ]; then
-        return 1
+    FEDORA_VERSION=$(rpm -E %fedora)
+    RHEL_VERSION=$(rpm -E %rhel)
+    CENTOS_VERSION=$(rpm -E %centos)
+    if [ "$FEDORA_VERSION" != "%fedora" ]; then
+        export BASE_OS_TYPE="fedora"
+        export BASE_OS_VERSION="$FEDORA_VERSION"
+        return
+    elif [ "$ALMA_VERSION" != "%almalinux" ]; then
+        export BASE_OS_TYPE="almalinux"
+        export BASE_OS_VERSION="$ALMA_VERSION"
+        return
+    elif [ "$CENTOS_VERSION" != "%centos" ]; then
+        export BASE_OS_TYPE="centos"
+        export BASE_OS_VERSION="$CENTOS_VERSION"
+        return
+    elif [ "$RHEL_VERSION" != "%rhel" ]; then
+        export BASE_OS_TYPE="rhel"
+        export BASE_OS_VERSION="$RHEL_VERSION"
+        return
     else
-        return 0
+        export BASE_OS_TYPE="unknown"
+        export BASE_OS_VERSION="unknown"
     fi
 }
