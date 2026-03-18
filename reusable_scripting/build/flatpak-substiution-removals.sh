26ec2a85674fb7b972097ae159620965615222f0
Rich Renomeron <rrenomeron+github@gmail.com>
2026-03-18 12:57:18 -0400

    chore: remove extra pkgs in alma

--- reusable_scripting/build/flatpak-substiution-removals.sh
+++ reusable_scripting/build/flatpak-substiution-removals.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/bash
 
 set -oeu pipefail
+source /ctx/oci/tr-osforge/build/helpers/utils.sh
 
-# These are not present in the latest silverblue
-# $DNF_CMD -y remove \
-# 	baobab \
-# 	gnome-calculator \
-# 	gnome-characters \
-# 	gnome-clocks \
-# 	gnome-font-viewer \
-# 	gnome-text-editor \
-# 	papers \
-# 	snapshot \
-# 	loupe \
-# 	PackageKit  \
-# 	PackageKit-command-not-found \
-# 	gnome-extensions-app
+if [ "$BASE_OS_TYPE" == "almalinux" ]; then
+	$DNF_CMD -y remove \
+		baobab \
+		gnome-calculator \
+		gnome-characters \
+		gnome-clocks \
+		gnome-font-viewer \
+		gnome-text-editor \
+		papers \
+		snapshot \
+		loupe \
+		PackageKit  \
+		PackageKit-command-not-found 
+	# 	gnome-extensions-app
+fi
 
 dnf -y remove \
 	gnome-disk-utility \
